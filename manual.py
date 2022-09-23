a250faff806d51a24bfb9522918b01a3d4b15d7b
SteamB23 <steamb23@outlook.com>
2022-09-23 23:45:30 +0900

    매뉴얼 코드 반자동화, data 병합에 따른 이중 체크아웃 삭제

--- manual.py
+++ manual.py
@@ -1,3 +1,16 @@
+from modulefinder import IMPORT_NAME
+
+import clipboard
 import main
 
-print(main.GetReport())
+while (1):
+    result = main.GetReport()
+    nospace = result.replace(" ","").replace("\n","").replace("\r","")
+    nospace_len = len(nospace)
+    print("공백제외 글자수: %d\n" % nospace_len)
+    print(result)
+    if (nospace_len < 200):
+        print("글자 수가 적습니다. 재시도합니다...\n")
+    else:
+        clipboard.copy(result)
+        break
